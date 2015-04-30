2a17b31edcbf43c86df5404bc9df1bc4eb77a2e0
Grégory <durdur07@hotmail.fr>
2015-04-30 11:18:21 +0200

    Création modèle version 1

---
+++ src/main/java/fr/univtlse3/Batiment.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.univtlse3;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Tempête
+ */
+public class Batiment{
+    ArrayList<Salle> salle;
+}
---
+++ src/main/java/fr/univtlse3/Machine.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.univtlse3;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Tempête
+ */
+public class Machine{
+    
+    ArrayList<InterfaceReseau> interfaceReseau;
+    ArrayList<InterfaceSerie> interfaceSerie;
+    ArrayList<InterfaceSansFil> interfaceSansFil;
+    ArrayList<UniteProcesseur> uniteProcesseur;
+}
