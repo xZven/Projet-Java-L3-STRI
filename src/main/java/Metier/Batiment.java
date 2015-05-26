ed0dab3e4bcf082f12b3d5f4f35c8b8f80cab833
Manu <Manu@130.120.214.138>
2015-05-26 10:56:29 +0200

    Modification javadoc: correction Typographie, harmonisation de la doc Identation code

--- src/main/java/Metier/Batiment.java
+++ src/main/java/Metier/Batiment.java
@@ -20,7 +20,8 @@ import javax.swing.JLabel;
  * </p>
  * @version 1.0, 2015, UPS.
  */
-public class Batiment extends JLabel{
+public class Batiment extends JLabel
+{
     
     private ArrayList<Salle> salles;
     private String nom;
@@ -34,7 +35,8 @@ public class Batiment extends JLabel{
      * @param localisation String: localisation (ou adresse)
      * @param description  String: description du batiment
      */
-    public Batiment(String name, String localisation, String description) {
+    public Batiment(String name, String localisation, String description) 
+    {
 		this.nom = name;
 		this.localisation = localisation;
                 this.description = description;
@@ -47,7 +49,8 @@ public class Batiment extends JLabel{
      * Constructeur de test pour JTree
      * @param name String: nom du batiment
      */
-    public Batiment(String name) {
+    public Batiment(String name) 
+    {
 		this.nom = name;
 		salles = new ArrayList<>();
                 
@@ -61,21 +64,23 @@ public class Batiment extends JLabel{
      * 
      * @return ArrayList de Salle.
      */
-    public ArrayList<Salle> getSalles() {
+    public ArrayList<Salle> getSalles()
+    {
         return salles;
     }
 
     /**
      * @param salles ArrayList de salle pour un batiment
      */
-    public void setSalles(ArrayList<Salle> salles) {
+    public void setSalles(ArrayList<Salle> salles)
+    {
         this.salles = salles;
     }
 
     /**
      * Ajouter une nouvelle salle au batiment
      * 
-     * @param salle permet d'ajouter une salle à l'ArrayList de la classe Batiment
+     * @param salle Salle: permet d'ajouter une salle à l'ArrayList de la classe Batiment
      */
     public void addSalle(Salle salle)
     {
@@ -85,20 +90,22 @@ public class Batiment extends JLabel{
     /**
      * Supprimer une salle au batiment.
      * 
-     * @param salle de type Salle
+     * @param salle Salle
      * @return boolean: TRUE si réussi; FALSE sinon
      */
     public boolean removeSalle(Salle salle) 
     {
-        if(this.salles.isEmpty() == true) {
-            // La n'a aucune salle.
+        if(this.salles.isEmpty() == true) 
+        {
+            // La salle est vide.
             
             /* On peut le vérifier dans la base de donnée */
             return true;
         }
         
-        /* Si la salles n'est pas présente dans l'ArrayList des salles */
-        if (!this.salles.contains(salle)) {
+        /* Si la salle n'est pas présente dans l'ArrayList des salles */
+        if (!this.salles.contains(salle)) 
+        {
                 System.out.println("Cette salle n'est pas dans ce batiment");
                 return false;
         } 
@@ -113,72 +120,75 @@ public class Batiment extends JLabel{
      * 
      * @return String: nom du batiment
      */
-    public String getNom() {
+    public String getNom() 
+    {
         return nom;
     }
 
     /**
-     * Définir un nom à un batiment
+     * Définir un nom à un batiment.
      * 
-     * @param nom nom à définir pour le batiment
+     * @param nom String: nom à définir pour le batiment.
      */
-    public void setNom(String nom) {
+    public void setNom(String nom) 
+    {
         this.nom = nom;
     }
 
     /**
      * Obtenir la desceription du batiment.
-     * @return String: Description du batiment
+     * @return String: Description du batiment.
      */
-    public String getDescription() {
+    public String getDescription() 
+    {
         return description;
     }
 
     /**
-     * @param description the description to set
+     * @param description String: Description à definir  pour le batiment.
      */
-    public void setDescription(String description) {
+    public void setDescription(String description) 
+    {
         this.description = description;
     }
 
     /**
-     * @return the location
+     * @return String: Localisation du batiment.
      */
-    public String getLocalisation() {
+    public String getLocalisation() 
+    {
         return localisation;
     }
 
     /**
      * Définir la localisation du batiment.
      * 
-     * @param localisation String: localisation à définir
+     * @param localisation String: Localisation à définir.
      */
-    public void setLocalisation(String localisation) {
+    public void setLocalisation(String localisation) 
+    {
         this.localisation = localisation;
     }
 
     /**
      * Obtenir seulement le nom du batiment(utilisé avec le JTree).
      * 
-     * @return String: nom du batiment
+     * @return String: nom du batiment.
      */
     @Override
-    public String toString() {
-		return (nom);
-	}
+    public String toString() 
+    {
+        return (nom);
+    }
     
     /**
      * Obtenir toutes les informations sur le batiment.
      * 
-     * @return String: nom, localisation, description du batiment
+     * @return String: nom, localisation, description du batiment.
      */
-    
-    /**
-     * Obtenir toutes les informations sur le batiment.
-     * 
-     * @return String: nom, localisation, description du batiment
-     */
-    public String FullScreen() {
+   
+    public String FullScreen() 
+    {
         return ("Nom: " + nom 
                 + "\nLocalisation:" + localisation 
                  + "\nDescription:" + description
